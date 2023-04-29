f3223d24255c47c4afcddb805891ecd7e1440d16
Vadim Kononov <vadim@konoson.com>
2023-04-29 09:44:58 -0500

    Fix an issue that did not detect stopped state.
    Previously, the script could not distinguish between stopped and paused states, causing the countup of inactivity minutes to increase even when the player was stopped. This led to the TV turning back on after the threshold was reached.
    
    Now the three states - paused, playing, stopped - are detected correctly.

--- kodi-auto-stop.sh
+++ kodi-auto-stop.sh
@@ -10,16 +10,35 @@ PAUSED_FILE="/tmp/kodi-minutes-paused.txt"
 # Set the duration threshold in minutes
 DURATION_THRESHOLD=30
 
-# Set the player ID to 1 (assumes the video player is currently active)
-PLAYER_ID=1
+# get the player ID
+RESPONSE=$(curl -s -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}' http://$KODI_HOST:$KODI_PORT/jsonrpc)
+PLAYER_ID=$(echo "$RESPONSE" | grep -o '"playerid":[0-9]*' | cut -d: -f2)
+
+# if player ID is empty, the video is stopped
+if [ "$PLAYER_ID" = "" ]; then
+  if [ -f "$PAUSED_FILE" ]; then
+    echo "0" > "$PAUSED_FILE"
+  fi
+
+  exit 0
+fi
 
 # Call the Player.GetProperties method to get the current player state
-RESPONSE=$(curl -s -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "Player.GetProperties", "params": {"playerid": '$PLAYER_ID', "properties": ["speed", "time"]}, "id": 1}' http://$KODI_HOST:$KODI_PORT/jsonrpc)
+RESPONSE=$(curl -s -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "Player.GetProperties", "params": {"playerid": '"$PLAYER_ID"', "properties": ["speed", "time"]}, "id": 1}' http://$KODI_HOST:$KODI_PORT/jsonrpc)
 
-# Parse the response to check if the video is paused
-PAUSED=$(echo "$RESPONSE" | grep -o '"speed":[0-9]*' | cut -d: -f2)
+# Parse the response to check if the video is paused or not playing
+SPEED=$(echo "$RESPONSE" | grep -o '"speed":[0-9]*' | cut -d: -f2)
 
-if [ "$PAUSED" -eq 0 ]; then
+# extract the values of hours, milliseconds, minutes and seconds using grep and awk
+HOURS=$(echo "$RESPONSE" | grep -o '"hours":[0-9]*' | awk -F: '{print $2}')
+MILLISECONDS=$(echo "$RESPONSE" | grep -o '"milliseconds":[0-9]*' | awk -F: '{print $2}')
+MINUTES=$(echo "$RESPONSE" | grep -o '"minutes":[0-9]*' | awk -F: '{print $2}')
+SECONDS=$(echo "$RESPONSE" | grep -o '"seconds":[0-9]*' | awk -F: '{print $2}')
+
+# calculate the sum of the values
+VIDEO_DURATION=$((HOURS + MILLISECONDS + MINUTES + SECONDS))
+
+if [ "$SPEED" -eq 0 ] && [ $VIDEO_DURATION -gt 0 ]; then
   # Video is paused, increment the paused duration in the file
   if [ -f "$PAUSED_FILE" ]; then
     # The paused file exists, read the current duration and increment it
@@ -29,7 +48,7 @@ if [ "$PAUSED" -eq 0 ]; then
     # Check if the paused duration has exceeded the duration threshold
     if [ "$PAUSED_DURATION" -ge "$DURATION_THRESHOLD" ]; then
       # Send a stop command to Kodi
-      curl -s -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "Player.Stop", "params": {"playerid": '$PLAYER_ID'}, "id": 1}' http://$KODI_HOST:$KODI_PORT/jsonrpc > /dev/null 2>&1
+      curl -s -H "Content-Type: application/json" -d '{"jsonrpc": "2.0", "method": "Player.Stop", "params": {"playerid": '"$PLAYER_ID"'}, "id": 1}' http://$KODI_HOST:$KODI_PORT/jsonrpc > /dev/null 2>&1
 
       # Reset the paused duration to 0
       echo "0" > "$PAUSED_FILE"
